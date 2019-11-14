67774863ea45549c82f9be52c3673c427bd52fcc
Angelo-Diniz <angelo.jose.7l@hotmail.com>
2019-11-14 18:37:28 -0300

    tratamento de excecao

--- js/acoesNovas.js
+++ js/acoesNovas.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
     $("#enviarEmail").click(function () {
-        if ($("#emailDoador").val() !== "" && $("#valorSelecionado option:selected").val() !== ""){
-            if(dispararEmail(email, valor)){
-                alert("Enviado!");
-                location.reload();
+
+        try {
+            if ($("#emailDoador").val() !== "" && $("#valorSelecionado option:selected").val() !== ""){
+
+                if(dispararEmail($("#emailDoador").val(), $("#valorSelecionado option:selected").val())){
+                    alert("Enviado!");
+                    location.reload();
+                }
+                else {
+                    alert("Desculpe! \nTivemos um problema. Tente mais tarde!");
+                    location.reload();
+                }
             }
-            else {
-                alert("Desculpe! \nTivemos um problema. Tente mais tarde!");
-                location.reload();
+            else{
+                alert("É necessário preencher todos os campos");
             }
+        } catch (error) {
+            console.log(error.message)
+            alert("Desculpe! \nTivemos um problema. Tente mais tarde!");
+            location.reload();
         }
-        else{
-            alert("É necessário preencher todos os campos");
-        }
+       
     });
 
 
