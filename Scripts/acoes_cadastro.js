bbf32240d6afba8add29cc97040d08f322d16d3d
Angelo-Diniz <angelo.jose.7l@hotmail.com>
2019-11-24 21:58:14 -0300

    request pessoa

--- Scripts/acoes_cadastro.js
+++ Scripts/acoes_cadastro.js
@@ -1,17 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#Cadastrar").click(function () {
         try {
-            if ($("#nome").val() !== ""
-            && $("#cpf").val() !== "" ){
-                if(cadastrarDoador($("#nome").val(), $("#email").val(), $("#cpf").val(), $("#endereco").val())){
+            if ($("#nome").val() !== "" &&
+                $("#cpf").val() !== "") {
+                if (cadastrarDoador($("#nome").val(), $("#email").val(), $("#cpf").val(), $("#endereco").val())) {
                     alert("Cadastrado!");
                     document.location.reload(true);
-                }
-                else {
+                } else {
                     alert("Desculpe! \nTivemos um problema. Tente mais tarde!");
                 }
-            }
-            else{
+            } else {
                 alert("Nome, CPF e Email são obritatórios");
             }
         } catch (error) {
@@ -24,37 +22,37 @@ $(document).ready(function(){
 
 function cadastrarDoador(nome, email, cpf, endereco) {
     try {
-        let webService =  services.find(servico => servico.identificador === "cadastrarDoador");
-        const Http = new XMLHttpRequest();
-        const url= webService.url;
-        Http.open("POST", url);
-        
-        Http.setRequestHeader("Content-Type", "application/json");
-        Http.setRequestHeader("Access-Control-Allow-Origin", "*");
-        Http.setRequestHeader("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-        Http.setRequestHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        let webService = services.find(servico => servico.identificador === "cadastrarDoador");
+        const request = new XMLHttpRequest();
+        const url = webService.url;
+        request.open("POST", url);
 
-        let dados= {
+        request.setRequestHeader("Content-Type", "application/json");
+
+        let dados = {
             "nome": nome,
             "cpf": cpf,
             "endereco": endereco || "Nao disponivel",
             "email": email
         }
-
-        Http.send(dados);
-        Http.onreadystatechange = (e) => {
-          console.log(Http.responseText)
+        request.send(dados);
+        request.onload = (e) => {
+            if (request.status === 201) {
+                console.log(request.statusText);
+            } else {
+                console.error(request.statusText);
+            }
         }
 
     } catch (error) {
-        console.log(JSON.stringify(error) + 'Response' + JSON.stringify(Http))
+        console.log(JSON.stringify(error) + 'Response' + JSON.stringify(request))
     }
 }
 
 const services = [{
-    identificador : "cadastrarDoador",
-    url : "https://api-dad.herokuapp.com/pessoa", 
-    token : "",
-    usuario : "",
-    senha : "",
-}]; 
+    identificador: "cadastrarDoador",
+    url: "https://api-dad.herokuapp.com/pessoa",
+    token: "",
+    usuario: "",
+    senha: "",
+}];
